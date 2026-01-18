9f0a652782d79dee7fd3f440bf37c42df4468d3b
Jude Asante Marfo <judeasantemarfo@gmail.com>
2026-01-18 17:17:07 +0000

    role based access

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -11,3 +11,12 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
 ]
+
+from django.urls import path
+from . import views
+
+urlpatterns += [
+    path('admin-view/', views.admin_view, name='admin_view'),
+    path('librarian-view/', views.librarian_view, name='librarian_view'),
+    path('member-view/', views.member_view, name='member_view'),
+]
