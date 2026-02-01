6d89735416309f05ed355be1069a07d68d63b62b
Jude Asante Marfo <judeasantemarfo@gmail.com>
2026-02-01 19:02:37 +0000

    Authentication and pemissions in DRF

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'api',
 ]
 
@@ -117,3 +118,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
+# REST Framework configuration
+# TokenAuthentication ensures that clients must provide a valid token
+# IsAuthenticated ensures only logged-in users can access API endpoints
