ef4f612f0d3e6a88f91593275aa43c79fecad1f6
jamarfo-oo24 <judeasantemarfo@gmail.com>
2026-02-08 15:20:53 +0000

    modified api/view.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,14 +1,11 @@
 from rest_framework import generics
-from rest_framework.permissions import (
-    AllowAny,
-    IsAuthenticatedOrReadOnly,
-    IsAuthenticated
-)
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 
 from .models import Book
 from .serializers import BookSerializer
+
 
 
 # ======================================================
