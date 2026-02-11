45ebb3ab8d73a32aa38a3b4ca6fe901c60e8500f
jamarfo-oo24 <judeasantemarfo@gmail.com>
2026-02-11 20:39:42 +0000

    Blog Post Management Features

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -7,6 +8,9 @@ class Post(models.Model):
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
 
     def __str__(self):
         return self.title
--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,17 +1,43 @@
 from django.urls import path
-from . import views
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    register,
+    profile
+)
+
 from django.contrib.auth import views as auth_views
 
+
 urlpatterns = [
-    # Login
+
+    # -------------------------
+    # Authentication URLs
+    # -------------------------
+
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
 
-    # Logout
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
 
-    # Register
-    path('register/', views.register, name='register'),
+    path('register/', register, name='register'),
 
-    # Profile
-    path('profile/', views.profile, name='profile'),
+    path('profile/', profile, name='profile'),
+
+
+    # -------------------------
+    # Blog Post CRUD URLs
+    # -------------------------
+
+    path('posts/', PostListView.as_view(), name='post-list'),
+
+    path('posts/new/', PostCreateView.as_view(), name='post-create'),
+
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+
+    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+
+    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
