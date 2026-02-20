0be9fb766c65511a664b6b4b7c2d228e7ece62d7
jamarfo-oo24 <judeasantemarfo@gmail.com>
2026-02-20 02:56:13 +0000

    Fix serializers to meet checker requirements

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,14 +1,28 @@
-from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
+User = get_user_model()
+
+
+# ==============================
+# User Registration Serializer
+# ==============================
 
 class RegisterSerializer(serializers.ModelSerializer):
+
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = get_user_model()
-        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
+        model = User
+        fields = [
+            'id',
+            'username',
+            'email',
+            'password',
+            'bio',
+            'profile_picture'
+        ]
 
     def create(self, validated_data):
 
@@ -19,6 +33,45 @@ class RegisterSerializer(serializers.ModelSerializer):
             bio=validated_data.get('bio', '')
         )
 
+        # create auth token automatically
         Token.objects.create(user=user)
 
         return user
+
+
+# ==============================
+# Login Serializer
+# ==============================
+
+class LoginSerializer(serializers.Serializer):
+
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+
+# ==============================
+# User Profile Serializer
+# ==============================
+
+class UserSerializer(serializers.ModelSerializer):
+
+    followers_count = serializers.SerializerMethodField()
+    following_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'username',
+            'email',
+            'bio',
+            'profile_picture',
+            'followers_count',
+            'following_count'
+        ]
+
+    def get_followers_count(self, obj):
+        return obj.followers.count()
+
+    def get_following_count(self, obj):
+        return obj.following.count()
