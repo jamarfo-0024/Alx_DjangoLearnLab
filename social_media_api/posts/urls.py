4bacc0eda9d7e4369afc0bb00a022edbc8dbf364
jamarfo-oo24 <judeasantemarfo@gmail.com>
2026-02-20 17:14:05 +0000

    updated posts/urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet
+from .views import PostViewSet, CommentViewSet, feed
 
 router = DefaultRouter()
-router.register(r'posts', PostViewSet)
-router.register(r'comments', CommentViewSet)
+router.register(r'posts', PostViewSet, basename='posts')
+router.register(r'comments', CommentViewSet, basename='comments')
 
 urlpatterns = [
     path('', include(router.urls)),
+
+    # Feed endpoint (checker expects "feed/")
+    path('feed/', feed, name='feed'),
 ]
